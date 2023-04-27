0b4330f874f759c6260155e6214e5549c2c15baf
Anish <anish.alwekar@gmail.com>
2023-04-27 22:27:46 +0530

    Made changes committing

--- src/main/java/com/azure/controller/AzureController.java
+++ src/main/java/com/azure/controller/AzureController.java
@@ -6,14 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.azure.model.Product;
-import com.azure.repository.AzureRepository;
-
 @RestController
 public class AzureController {
 
-	@Autowired
-	private AzureRepository repo;
+//	@Autowired
+//	private AzureRepository repo;
 
 	@GetMapping("/azure")
 	public String helloAzure() {
--- src/main/java/com/azure/repository/AzureRepository.java
+++ src/main/java/com/azure/repository/AzureRepository.java
@@ -1,11 +1,11 @@
-package com.azure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.azure.model.Product;
-
-@Repository
-public interface AzureRepository extends JpaRepository<Product,Long>{
-
-}
+//package com.azure.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.azure.model.Product;
+//
+//@Repository
+//public interface AzureRepository extends JpaRepository<Product,Long>{
+//
+//}
